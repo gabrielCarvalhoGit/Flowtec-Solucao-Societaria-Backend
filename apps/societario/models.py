6a9e5cc682122dbcd204abb95241f576739803a0
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-10-16 00:26:49 -0300

    Definindo modelo base da tabela AberturaEmpresa.

--- apps/societario/models.py
+++ apps/societario/models.py
@@ -1,21 +1,25 @@
 import uuid
 from django.db import models
 
+from apps.contabilidades.models import Contabilidade
+
 
 class AberturaEmpresa(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    contabilidade = models.ForeignKey(Contabilidade, on_delete=models.CASCADE)
 
     nome_primario = models.CharField(max_length=100)
-    nome_secundario = models.CharField(max_length=100)
-    nome_terciario = models.CharField(max_length=100)
+    nome_secundario = models.CharField(max_length=100, blank=True, null=True)
+    nome_terciario = models.CharField(max_length=100, blank=True, null=True)
 
-    atividade_principal = models.CharField(max_length=150)
+    cep = models.CharField(max_length=9, blank=True, null=True)
+    endereco = models.CharField(max_length=150, blank=True, null=True)
 
-    cep = models.CharField(max_length=9)
-
-    responsavel = models.CharField(max_length=100)
-    email = models.EmailField(unique=False)
-    telefone = models.CharField(max_length=12)
+    telefone = models.CharField(max_length=12, blank=True, null=True)
+    email = models.EmailField(unique=False, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.nome_primario} - {self.contabilidade.nome}'

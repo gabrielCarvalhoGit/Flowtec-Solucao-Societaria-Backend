da2859ce3420f693eafbacebd787d01e81e416d6
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-10-16 00:35:53 -0300

    Alterando tabela de usuários no banco e configurando django cors-headers para integração com o front.

--- solucao_societaria/settings.py
+++ solucao_societaria/settings.py
@@ -155,3 +155,9 @@ CORS_ALLOWED_ORIGINS = [
 
 CNPJ_API_KEY = os.getenv('CNPJ_API_KEY')
 CNPJ_API_URL = os.getenv('CNPJ_API_URL')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True

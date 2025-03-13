53b9929af5c682d63a68c5540ce7576014f94978
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2025-03-13 09:25:53 -0300

    Add static root for nginx.

--- solucao_societaria/settings.py
+++ solucao_societaria/settings.py
@@ -150,6 +150,8 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 AUTH_USER_MODEL = 'accounts.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

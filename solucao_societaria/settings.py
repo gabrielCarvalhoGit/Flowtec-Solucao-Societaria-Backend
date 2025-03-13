cab0b844de380f3022c050d9ebe4a99592ad5c63
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2025-03-13 09:48:51 -0300

    Revert "Revert "Add static url.""
    This reverts commit b3fe4c5de9229088eebfb0fe8d3295cf46c51f07.

--- solucao_societaria/settings.py
+++ solucao_societaria/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 AUTH_USER_MODEL = 'accounts.User'
